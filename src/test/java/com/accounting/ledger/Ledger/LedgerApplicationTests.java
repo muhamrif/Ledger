2dd4a9f9ded9cf7761f4fb6cb3ae458fecbecedf
JalesiaGriffin <jgriffin@appdev.yearup.org>
2024-01-17 10:22:58 -0500

    initial commit

---
+++ src/test/java/com/accounting/ledger/Ledger/LedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.ledger.Ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
