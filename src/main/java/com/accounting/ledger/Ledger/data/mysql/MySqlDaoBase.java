0258225802126ab4ed745e5cdd83d786cbf53ca5
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2024-01-17 13:13:51 -0500

    skeleton and db SQL file

---
+++ src/main/java/com/accounting/ledger/Ledger/data/mysql/MySqlDaoBase.java
@@ -0,0 +1,20 @@
+package com.accounting.ledger.Ledger.data.mysql;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class MySqlDaoBase
+{
+    private final DataSource dataSource;
+
+    public MySqlDaoBase(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
+
+    protected Connection getConnection() throws SQLException
+    {
+        return dataSource.getConnection();
+    }
+}
---
+++ src/main/java/com/accounting/ledger/Ledger/models/Transaction.java
@@ -0,0 +1,4 @@
+package com.accounting.ledger.Ledger.models;
+
+public class Transaction {
+}
