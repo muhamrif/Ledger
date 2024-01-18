2aef6506416c2123a8d62cdd2b41fbc8d3428386
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2024-01-17 22:30:02 -0500

    get all transactions + maprow

--- src/main/java/com/accounting/ledger/Ledger/data/TransactionDao.java
+++ src/main/java/com/accounting/ledger/Ledger/data/TransactionDao.java
@@ -1,4 +1,15 @@
 package com.accounting.ledger.Ledger.data;
 
+import com.accounting.ledger.Ledger.models.Transaction;
+
+import java.util.List;
+
 public interface TransactionDao {
+
+     List<Transaction> getAll();
+    Transaction getById(int id);
+    Transaction addTransaction(Transaction transaction);
+    void update(int id, Transaction transaction);
+    void delete(int id);
+
 }
--- src/main/java/com/accounting/ledger/Ledger/data/mysql/MyTransactionDao.java
+++ src/main/java/com/accounting/ledger/Ledger/data/mysql/MyTransactionDao.java
@@ -1,17 +1,47 @@
 package com.accounting.ledger.Ledger.data.mysql;
 
+import com.accounting.ledger.Ledger.data.TransactionDao;
 import com.accounting.ledger.Ledger.models.Transaction;
-import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
-@AllArgsConstructor
-public class MyTransactionDao {
-    private DataSource dataSource;
+public class MyTransactionDao extends MySqlDaoBase implements TransactionDao {
+
+
+    @Autowired
+    public MyTransactionDao(DataSource dataSource) {
+        super(dataSource);
+    }
 
     public List<Transaction> getAll(){
-        return null;
+        List<Transaction> transactions = new ArrayList<>();
+
+        String sql = "SELECT * FROM transactions";
+
+        try (Connection connection = this.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    Transaction transaction = mapRow(resultSet);
+                    transactions.add(transaction);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return transactions;
     }
 
     public Transaction getById(int id){
@@ -25,4 +55,16 @@ public class MyTransactionDao {
     public void update(int id, Transaction transaction){}
 
     public void delete(int id){}
+
+    private Transaction mapRow(ResultSet resultSet) throws SQLException {
+
+
+            String description = resultSet.getString("transaction_description");
+            String vendor = resultSet.getString("vendor_name");
+            LocalDate date = resultSet.getDate("transaction_date").toLocalDate();
+            LocalTime time = resultSet.getTime("transaction_time").toLocalTime();
+            double amount = resultSet.getDouble("amount");
+
+            return new Transaction(description, vendor, date, time, amount);
+    }
 }
