c12173704c8788d3765825748d66d2fd9f038101
JalesiaGriffin <jgriffin@appdev.yearup.org>
2024-01-19 10:33:28 -0500

    created profile model

---
+++ src/main/java/com/accounting/ledger/Ledger/models/Profile.java
@@ -0,0 +1,21 @@
+package com.accounting.ledger.Ledger.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile
+{
+    private int userId;
+    private String firstName = "";
+    private String lastName = "";
+    private String phone = "";
+    private String email = "";
+    private String address = "";
+    private String city = "";
+    private String state = "";
+    private String zip = "";
+}
